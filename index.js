1f067d9d2c8d22c6e6f20d71984a1f0c681291f4
netakaya <netakayainesamaya@gmail.com>
2024-09-18 05:28:38 +0300

    Залил код в новый репозиторий

--- index.js
+++ index.js
@@ -3,4 +3,19 @@ const { Bot } = require('./src');
 
 const token = process.env.QUERY_ID;
 const blumBot = new Bot('blum', token, 'wKl9ShrBkV');
+
+const express = require('express');
+const app = express();
+
+// Твой код для работы бота
+
+// Это часть для прослушивания порта, чтобы Render был доволен
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.send('Bot is running');
+});
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
+
 blumBot.start().then(r => {});
